d604493abf2f90b8bfb2e03ed7230e25e0335720
aster <137767097+aster-void@users.noreply.github.com>
2024-07-08 19:24:23 +0900

    make red/nodejs work

---
+++ red/nodejs/main.js
@@ -0,0 +1,58 @@
+const { setTimeout } = require("timers/promises");
+const {
+  Worker,
+  workerData,
+  parentPort,
+  isMainThread,
+} = require("worker_threads");
+
+async function main() {
+  require("dotenv").config();
+  const count = process.env.COUNT ?? 5;
+  const sleep = (process.env.SLEEP_BETWEEN ?? 0) * 1000;
+  const result = await splitWorkers({
+    count,
+    sleep,
+  });
+  console.log(result);
+}
+
+async function worker() {
+  if (workerData.sleep !== 0) {
+    console.log(`workerData.count === ${workerData.count}`);
+    await setTimeout(workerData.sleep);
+  }
+  if (workerData.count <= 1) {
+    parentPort.postMessage(workerData.count);
+    return
+  }
+  const one = splitWorkers({
+    count: workerData.count - 1,
+    sleep: workerData.sleep,
+  });
+  const two = splitWorkers({
+    count: workerData.count - 2,
+    sleep: workerData.sleep,
+  });
+  parentPort.postMessage(await one + await two);
+}
+
+if (isMainThread) {
+  main();
+} else {
+  worker();
+}
+
+function splitWorkers(workerData) {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(__filename, {
+      workerData,
+    });
+    worker.on('message', resolve);
+    worker.on('error', reject);
+    worker.on('exit', (code) => {
+      if (code !== 0)
+        reject(new Error(`Worker stopped with exit code ${code}`));
+    });
+  })
+}
