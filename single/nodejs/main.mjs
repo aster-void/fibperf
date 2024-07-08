bd1b2bed262149fc7e7a5b83c962afd2e17c09ce
aster <137767097+aster-void@users.noreply.github.com>
2024-07-08 21:59:07 +0900

    remove async/await from single/nodejs

--- single/nodejs/main.mjs
+++ single/nodejs/main.mjs
@@ -5,19 +5,19 @@ dotenv.config();
 const sleep = (process.env.SLEEP ?? 0) * 1000;
 const count = process.env.COUNT ?? 10;
 
-const result = await fib(count);
+const result = fib(count);
 console.log(result);
 
-async function fib(count) {
+function fib(count) {
   if (sleep !== 0) {
     console.log(`running fib(${count})`);
-    await setTimeout(sleep);
+    setTimeout(sleep);
   }
   if (count <= 1) {
     return count;
   }
   const one = fib(count - 1);
   const two = fib(count - 2);
-  return await one + await two;
+  return one + two;
 }
 
